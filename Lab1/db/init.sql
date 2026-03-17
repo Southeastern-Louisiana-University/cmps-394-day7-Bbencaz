fd5f8734d689bd93a78f38743207e8743fe6c398
Bbencaz <brayden.bencaz@selu.edu>
2026-03-17 16:33:52 -0500

    Assignment completed

--- Lab1/db/init.sql
+++ Lab1/db/init.sql
@@ -1,8 +1,9 @@
 -- Create the messages table
-CREATE TABLE IF NOT EXISTS messages (
-    id      SERIAL PRIMARY KEY,
+CREATE TABLE messages (
+    ID      SERIAL PRIMARY KEY,
     content TEXT NOT NULL
 );
 
 -- Seed an initial message
-INSERT INTO messages (content) VALUES ('Hello, World!');
+INSERT INTO messages (content) 
+VALUES ('Have a great day!');
